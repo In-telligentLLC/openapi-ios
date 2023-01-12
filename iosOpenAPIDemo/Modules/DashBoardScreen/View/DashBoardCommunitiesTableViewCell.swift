306f1c6c657c70905794497e61e221aa50235189
Likhitha <likhitha.narayana@senecaglobal.com>
2023-01-12 12:43:03 +0530

    P000851903-4521- Commiting after implementing markOpened method for notification service extension

--- iosOpenAPIDemo/Modules/DashBoardScreen/View/DashBoardCommunitiesTableViewCell.swift
+++ iosOpenAPIDemo/Modules/DashBoardScreen/View/DashBoardCommunitiesTableViewCell.swift
@@ -13,7 +13,6 @@ class DashBoardCommunitiesTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
